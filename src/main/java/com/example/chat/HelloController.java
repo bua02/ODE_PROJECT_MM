f8e59e364a339f6a63878544d83c3a626de54124
Bernhard Bauer <ic22b023@technikum-wien.at>
2023-01-09 19:16:38 +0100

    responsive GUI

--- src/main/java/com/example/chat/HelloController.java
+++ src/main/java/com/example/chat/HelloController.java
@@ -1,15 +1,14 @@
 package com.example.chat;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class HelloController {
     @FXML
@@ -27,6 +26,7 @@ public class HelloController {
     private List<String> messageList = new ArrayList<>();
 
 
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
@@ -36,9 +36,8 @@ public class HelloController {
     public void onSendButtonPress(ActionEvent e){
         String message = tfMessage.getText();
         messageList.add(message);
-        taChatHistory.clear();
         for (String currentMessage:messageList) {
-            taChatHistory.appendText(currentMessage.toString() + "\n");
+            messageList.add(currentMessage.toString() + "\n");
         }
     }
 }
