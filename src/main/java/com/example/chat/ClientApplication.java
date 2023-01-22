556aeb4bbb7bb251e28fcf8e09152e96218aa00b
reti18 <retiai@outlook.com>
2023-01-22 19:13:17 +0100

    add send button and change window size

--- src/main/java/com/example/chat/ClientApplication.java
+++ src/main/java/com/example/chat/ClientApplication.java
@@ -11,7 +11,7 @@ public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("client-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Messenger");
         stage.setScene(scene);
         stage.show();
